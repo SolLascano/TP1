a6fd26b9d402946e6b05b7d60d06833200905767
SolLascano <reparacion.nehuen@gmail.com>
2019-04-28 14:51:09 -0300

    Se agregó código en test, con los test requeridos en TP.

--- Tp1/Test/ar/edu/unlam/pb2/DiscoTest/DiscoTest.java
+++ Tp1/Test/ar/edu/unlam/pb2/DiscoTest/DiscoTest.java
@@ -2,7 +2,46 @@ package ar.edu.unlam.pb2.DiscoTest;
 
 import org.junit.*;
 public class DiscoTest {
-
+@Test
+	public void testQueVerifiqueElPerimetroInterior() {
+		
+		Double radioInterior = 2.0;
+		Double radioExterior = 5.0;
+		
+		Disco nuevoPerimetroInteriorDiscoUno = new Disco(radioInterior,radioExterior);
+		
+		Double perimetroInterior = nuevoPerimetroInteriorDiscoUno.calcularPerimetroInteriorDisco(radioInterior);
+		Double perimetroInteriorEsperado = 12.566370614359172;
+		
+		Assert.assertEquals(perimetroInteriorEsperado, perimetroInterior);
+		
+	}
+	
+	@Test
+	public void testQueVeifiqueElPerimetroExterior() {
+		Double radioInterior = 2.0;
+		Double radioExterior = 5.0;
+		
+		Disco nuevoPerimetroExteriorDiscoUno = new Disco(radioInterior,radioExterior);
+		
+		Double perimetroExterior = nuevoPerimetroExteriorDiscoUno.calcularPerimetroExteriorDisco(radioExterior);
+		Double perimetroExteriorEsperado = 31.415926535897932;
+		Assert.assertEquals(perimetroExteriorEsperado, perimetroExterior);
+	}
+	
+	@Test 
+	public void TestQueVerifiqueLaSuperficieTotalDelDisco() {
+		
+		Double radioInterior = 2.0;
+		Double radioExterior = 5.0;
+		
+		Disco nuevoSuperficieTotalDiscoUno = new Disco(radioInterior , radioExterior);
+		
+		Double superficieTotal = nuevoSuperficieTotalDiscoUno.superficieTotalDelDisco();
+		Double superficieTotalEsperada = 74.53981633974483;
+		
+		Assert.assertEquals(superficieTotalEsperada, superficieTotal);
+	}
 }
 
 
